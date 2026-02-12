7b7e49e2d563259097f96ecf8004628fb2ea3e3b
kauanlsbarreto <kauanlsbarreto@hotmail.com>
2026-02-12 13:32:59 -0300

    fix: explicit open-next cloudflare-node configuration

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,12 +5,24 @@ const config: OpenNextConfig = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
   },
   edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 };
 
 export default config;
