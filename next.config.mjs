20ba8826dd0a2eaabf7bd71981321a2b0a583ab7
kauanlsbarreto <kauanlsbarreto@hotmail.com>
2026-02-12 13:45:43 -0300

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', 
+  output: 'standalone',
+  bundlePagesRouterDependencies: true, 
+  serverExternalPackages: ['mysql2'],
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  }
-}
+  },
+};
 
-export default nextConfig
+export default nextConfig;
