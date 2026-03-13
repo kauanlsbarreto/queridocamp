f22efba7c4fbb045c33cb7fb37f3329086eea57f
kauanlsbarreto <kauanlsbarreto@hotmail.com>
2026-03-12 21:27:58 -0300

    att

--- components/LiveMatchesController.tsx
+++ components/LiveMatchesController.tsx
@@ -1,8 +1,15 @@
 "use client"
 
+import { usePathname } from "next/navigation";
 import LiveMatchesModal from "./LiveMatchesModal";
 
 const LiveMatchesController = () => {
+    const pathname = usePathname();
+
+    if (pathname?.startsWith("/overlay")) {
+        return null;
+    }
+
     return <LiveMatchesModal />;
 };
 
