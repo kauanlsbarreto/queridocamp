9dc6c1a77ea63ca969e36cd5ed3edcde3c2547e4
kauanlsbarreto <kauanlsbarreto@hotmail.com>
2026-04-05 22:56:13 -0300

    agtt

--- app/api/loja/pagamento/webhook/route.ts
+++ app/api/loja/pagamento/webhook/route.ts
@@ -1,1 +1,3 @@
-export { dynamic, GET, POST } from "@/app/api/mercadopago/webhooks/route";
+export const dynamic = "force-dynamic";
+
+export { GET, POST } from "@/app/api/mercadopago/webhooks/route";
