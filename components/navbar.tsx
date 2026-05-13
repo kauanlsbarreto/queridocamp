bcd91f80c842cdef99dc0a69ba7d11a032e2af30
kauanlsbarreto <kauanlsbarreto@hotmail.com>
2026-05-12 22:04:09 -0300

    att

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -197,6 +197,7 @@ const Navbar = ({ user, onAuthChange }: NavbarProps) => {
                   Copa Draft<ChevronDown size={14} />
                 </button>
                 <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 w-48 bg-[#060D15]/95 backdrop-blur-xl border border-gold/20 rounded-xl overflow-hidden shadow-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 transform origin-top flex flex-col p-4 text-center">
+                  <DropdownLink href="/copadraft/palpites">Palpites</DropdownLink>
                   <DropdownLink href="/copadraft/times">Times</DropdownLink>
                   <DropdownLink href="/copadraft/classificacao">Classificacao</DropdownLink>
                   <DropdownLink href="/copadraft/stats">Estatísticas</DropdownLink>
